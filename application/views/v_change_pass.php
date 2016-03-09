71aa8080bdd9a7ce6f91db363ce10bd5f3901bbc
Paulo Lieuthier <paulolieuthier@gmail.com>
2016-03-09 11:11:45 -0300

    Fix change password form

--- application/views/v_change_pass.php
+++ application/views/v_change_pass.php
@@ -12,10 +12,21 @@ if (!isset($is_admin)) $is_admin = FALSE;
 <pre>
 <h1>Mudança de senha</h1>
 É necessário que sua senha contenha no mínimo 4 caracteres. Digite no campo de confirmação a nova senha também, e memorize-a. No momento não é possível recuperar sua senha sem a ajuda de um monitor. Caso necessite, entre em contato com a monitoria.
-<?=form_open(base_url('/index.php/home/change_pass'))?><div id="register_block">
-Senha atual: <input name='current_pass' type="password" value="" type="text" style="position: absolute; right: 0px; width: 200px;" />
-Nova senha: <input name='new_pass' value="" type="password" style="position: absolute; right: 0px; width: 200px;" onchange="document.getElementById('email_form').value = (this.value)+'@cin.ufpe.br'"/>
-Confirme: <input name='confirm_pass' value="" type="password" style="position: absolute; right: 0px; width: 200px;" />
-<input type="submit" value="Alterar" style="position: absolute; right: 0px;"/></form></div>
-
+<?=form_open(base_url('/index.php/home/change_pass'))?>
+<div id="register_block">
+    <table>
+        <tr>
+            <td>Senha atual:</td>
+            <td><input name='current_pass' type="password" value="" type="text" /></td>
+        </tr>
+        <tr>
+            <td>Nova senha:</td>
+            <td><input name='new_pass' value="" type="password" /></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="Alterar" /></td>
+        </tr>
+    </table>
+</div>
+</form>
 </pre>
